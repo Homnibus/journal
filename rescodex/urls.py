cd3b911f78c948d5cb33ebc71e7ab4e9ab8b9def
Homnibus <eseunes@gmail.com>
2018-08-03 18:22:43 +0200

    fix urls
    fix connexion urls

--- rescodex/urls.py
+++ rescodex/urls.py
@@ -8,8 +8,8 @@ from projets import views as journal_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('connexion/', auth_views.login, {'template_name': 'projets/connexion.html'}, name='connexion'),
-    path('deconnexion/', auth_views.logout, {'template_name': 'projets/deconnexion.html'}, name='deconnexion'),
-    path('accueil/', journal_views.afficher_derniers_codex, name='accueil'),	
+    path('connexion/',auth_views.LoginView.as_view(template_name='projets/connexion.html'),name='connexion'),
+    path('deconnexion/',auth_views.LogoutView.as_view(template_name='projets/deconnexion.html'),name='deconnexion'),
+    path('accueil/', journal_views.afficher_derniers_codex, name='accueil'),
     path('projets/', include('projets.urls')),
-]  + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
